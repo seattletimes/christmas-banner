33e646dcd7299ba1a9e4c9f035559037e020286a
Thomas Wilburn <news@thomaswilburn.net>
2016-12-20 15:50:35 -0800

    Ain't no party like a higher-order function party...
    ...because a higher-order function party can easily abstract over
    asynchronous iteration problems.

--- src/js/main.js
+++ src/js/main.js
@@ -9,17 +9,6 @@ var $ = require("./lib/qsa");
 
 var container = document.querySelector(".svg-container");
 
-/*
-Start with cranes, mountain line, water line, boat and Smith Tower.
-Then Merry Christmas draws in.
-Glow turns on and 2016 pops in with glow.
-Boat start its movement across the water.
-
-Need polylines converted to paths
-Star_6 is not named correctly
-Christmas is out of order
-*/
-
 var order = [
   "Both_cranes",
   "Water_line",
@@ -30,37 +19,31 @@ var order = [
   "_2016"
 ];
 
-var stage = 0;
-
-var draw = function(done) {
-
+var each = function(a, fn, c) {
+  var i = 0;
   var step = function() {
-    var id = order[stage];
-    if (!id) return done();
-    var paths = $(`#${id} path, #${id} polyline, #${id} polygon, #${id} line`);
-    var i = 0;
-
-    var next = function() {
-      var path = paths[i];
-      var length = path.getTotalLength ? path.getTotalLength() : 10;
-      var time = length;
-      savage(path).draw(time);
-      path.style.opacity = 1;
+    if (i == a.length) return c();
+    fn(a[i], function() {
       i++;
-      if (i == paths.length) {
-        i = 0;
-        stage++;
-        return setTimeout(step, time);
-      }
-      setTimeout(next, time);
-    }
-
-    next();
-  };
+      step();
+    });
+  }
 
   step();
 };
 
-draw(function() {
+each(order, function(id, done) {
+
+  var paths = $(`#${id} path, #${id} polyline, #${id} polygon, #${id} line`);
+
+  each(paths, function(path, next) {
+    var length = path.getTotalLength ? path.getTotalLength() : 10;
+    var time = length;
+    savage(path).draw(time);
+    path.style.opacity = 1;
+    setTimeout(next, time);
+  }, done)
+
+}, function() {
   container.classList.add("warm");
-});
+})
